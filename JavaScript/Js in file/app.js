93e7ecc19d37497eb37240a260b7ac88709cb1f4
VasiliIgnatenko <30416299+VasiliIgnatenko@users.noreply.github.com>
2018-11-22 22:17:48 +0300

    js

--- JavaScript/Js in file/app.js
+++ JavaScript/Js in file/app.js
@@ -1,6 +1,11 @@
 var userFirstName = prompt("What is your first name?");
 var userLastName = prompt("What is your last name?");
 var userAge = prompt("What is your Age?");
+var countEar = prompt("What is your Ears?");
+var countDay;
+
+countDay = countEar * 365;
 
 console.log("Your full name is " + userFirstName + " " + userLastName);
 console.log("You are " + userAge + " years old");
+console.log("Count ear " + countEar + " = " + countDay + " count day");
