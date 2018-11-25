684d7bb4bdefaff2d2c92549279c37285c36c92b
VasiliIgnatenko <30416299+VasiliIgnatenko@users.noreply.github.com>
2018-11-26 00:13:47 +0300

    for loops

---
+++ JavaScript/For Loops/app.js
@@ -0,0 +1,26 @@
+for(var i = 0; i < 16; i+=8) {
+	console.log(i);
+}
+
+var str = "ahceclwlxo";
+
+for(var i = 0; i < str.length; i+=2) {
+	console.log(str[i]);
+}
+
+console.log("PRINTING ALL NUMBERS BETWEEN -10 AND 19");
+for(var i = -10; i < 20; i+=1) {
+	console.log(i);
+}
+
+console.log("PRINTING ALL EVEN BETWEEN 10 AND 40");
+
+for(var i = 10; i < 40; i+=2) {
+	console.log(i);
+}
+
+console.log("PRINTING ALL ODD NUMBERS BETWEEN 300 AND 333");
+for(var i = 300; i < 333; i++) {
+	console.log(i);
+}
+
--- JavaScript/Loops/app.js
+++ JavaScript/Loops/app.js
@@ -1,13 +1,45 @@
-var num = -11;
+console.log("PRINTING ALL NUMBERS BETWEEN -10 AND 19");
+var counter = -10;
 
-while(num < 41) {
-    console.log(num + 1);
-    num++;
+while(counter < 20){
+	console.log(counter);
+	counter++;
 }
 
-var num = 10;
+console.log("PRINTING ALL EVEN BETWEEN 10 AND 40");
+var counter = 10;
 
-while(num < 40) {
-    console.log(num + 2);
-    num +=2;
+while(counter <= 40){
+	console.log(counter);
+	counter+=2;
+}
+
+// while(counter <= 40){
+// 	if(counter % 2 === 0){
+// 		console.log(counter);
+// 	}
+// 	counter+=1;
+// }
+
+console.log("PRINTING ALL ODD NUMBERS BETWEEN 300 AND 333");
+var counter = 300;
+
+while(counter <= 333){
+	if(counter % 2 !== 0){
+		console.log(counter);
+	}
+	counter+=1;
+}
+
+
+
+
+console.log("PRINTING ALL NUMBERS DIVISIBLE BY 5 AND 3 BETWEEN 5 AND 50");
+var counter = 5;
+
+while(counter <= 50){
+	if(counter % 5 === 0 && counter % 3 === 0){
+		console.log(counter);
+	}
+	counter+=1;
 }
