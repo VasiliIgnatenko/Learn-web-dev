1523608e24a918af977aca3cca97ce20d497909a
VasiliIgnatenko <30416299+VasiliIgnatenko@users.noreply.github.com>
2018-11-26 23:54:09 +0300

    add js functions

---
+++ JavaScript/JS Functions/app.js
@@ -0,0 +1,20 @@
+//function isEven(x) {
+//	if(x % 2 === 0) {
+//		console.log(x +" true");
+//	} else{
+//		console.log(x +" false");
+//	}
+//}
+function isEven(x) {
+	return x % 2 === 0;
+}
+
+
+function factorial(f) {
+	return f ? f * factorial(f - 1) : 1;
+}
+	console.log(f);
+
+function kebabToSnake(str) {
+	console.log( str .replace ("-", "_") );
+}
