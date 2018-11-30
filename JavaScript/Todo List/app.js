4eceb2245a0e512edefed7cc880df410b9cebbb6
VasiliIgnatenko <30416299+VasiliIgnatenko@users.noreply.github.com>
2018-11-30 12:49:43 +0300

    Todo List complete

--- JavaScript/Todo List/app.js
+++ JavaScript/Todo List/app.js
@@ -1,19 +1,39 @@
 var todos = ["Buy New Turtle"];
 
-var input = prompt("What would you to do?");
+var input = prompt("What would you like to do?");
 
-while(input !== "quit") {
-
+while(input !== "quit"){
+	//handle input
 	if(input === "list") {
-		todos.forEach(function(todo, i) {
-		console.log(todo);
-		});
-		
+		printList();
 	} else if(input === "new") {
-		var newTodo = prompt("Enter new todo");
-		todos.push(newTodo);
-	} 
-
-	input = prompt("What would you to do?");
+		addTodo();
+	} else if(input === "delete") {
+		deleteTodo();
+	}
+	//ask again for new input
+	input = prompt("What would you like to do?");
 }
 console.log("OK, YOU QUIT THE APP");
+
+function printList() {
+	console.log("**********");
+	todos.forEach(function(todo, index){
+		console.log(index + ": " + todo);
+	});
+	console.log("**********");
+}
+
+function addTodo(){
+	//ask for new todo
+	var newTodo = prompt("Enter new todo");
+	//add to todos array
+	todos.push(newTodo);
+	console.log(newTodo + " added to list")
+}
+
+function deleteTodo(){
+	var index = prompt("Enter index of todo to delete");
+	todos.splice(index, 1);
+	console.log("Todo Removed")
+}
