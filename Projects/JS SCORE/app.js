c65a1ec7f686b3ab770c907e8559b837a418c3d7
VasiliIgnatenko <30416299+VasiliIgnatenko@users.noreply.github.com>
2018-12-03 23:45:17 +0300

    Score part1

---
+++ Projects/JS SCORE/app.js
@@ -0,0 +1,38 @@
+var p1Button = document.querySelector("#p1");
+var p2Button = document.querySelector("#p2");
+var reset = document.querySelector("#p3");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
+
+
+p1Button.addEventListener("click", function(){
+    if(!gameOver){
+        p1Score++;
+        if(p1Score === winningScore){
+            gameOver = true;
+        }
+        p1Display.textContent = p1Score;
+    }
+});
+
+p2Button.addEventListener("click", function(){
+    if(!gameOver){
+        p2Score++;
+        if(p2Score === winningScore){
+            gameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    }
+});
+
+reset.addEventListener("click", function(){
+    p1Display.textContent = p1Score = 0;
+    p2Display.textContent = p2Score = 0;
+    gameOver = true;
+});
+
+
