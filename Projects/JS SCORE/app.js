fe607c9d9bfd87090f22d71810bd9cba9f935d74
VasiliIgnatenko <30416299+VasiliIgnatenko@users.noreply.github.com>
2018-12-04 23:23:40 +0300

    JS SCORE end

--- Projects/JS SCORE/app.js
+++ Projects/JS SCORE/app.js
@@ -1,8 +1,10 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.querySelector("#p2");
-var reset = document.querySelector("#p3");
+var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -13,6 +15,7 @@ p1Button.addEventListener("click", function(){
     if(!gameOver){
         p1Score++;
         if(p1Score === winningScore){
+            p1Display.classList.add("winner");
             gameOver = true;
         }
         p1Display.textContent = p1Score;
@@ -23,16 +26,30 @@ p2Button.addEventListener("click", function(){
     if(!gameOver){
         p2Score++;
         if(p2Score === winningScore){
+            p2Display.classList.add("winner");
             gameOver = true;
-        }
+        } 
         p2Display.textContent = p2Score;
     }
 });
 
-reset.addEventListener("click", function(){
-    p1Display.textContent = p1Score = 0;
-    p2Display.textContent = p2Score = 0;
-    gameOver = true;
+resetButton.addEventListener("click", function(){
+    reset();
 });
 
+function reset(){
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+}
+
+numInput.addEventListener("change", function(){
+    winningScoreDisplay.textContent = this.value;
+    winningScore = Number(this.value);
+    reset();
+});
 
