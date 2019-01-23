ea728ccb4d643ef43e7beb552b344c437d8188f1
VasiliIgnatenko <30416299+VasiliIgnatenko@users.noreply.github.com>
2019-01-23 22:05:14 +0300

    1

--- Projects/TODO List/assets/js/app.js
+++ Projects/TODO List/assets/js/app.js
@@ -1,20 +1,9 @@
+// Check off specific todos by clicking
 $("li").click(function(){
     $(this).toggleClass("completed");
 });
 
-//if li is gray
-//if($(this).css("color") === "rgb(128, 128, 128)") {
-    //turn it black
-    //$(this).css({
-    //    color: "black",
-    //    textDecoration: "none"
-    //});
-//}  
-//else
-//else {
-    //turn it gray
-//    $(this).css({
-//        color: "gray",
-//        textDecoration: "line-through"
-//    });
-//}
+//Click on X to delete Todo
+$("span").click(function(){
+    alert("clicked");
+});
