9dc6f94a1fa3c780d423931006bac88f60ef2278
VasiliIgnatenko <30416299+VasiliIgnatenko@users.noreply.github.com>
2019-01-26 14:22:58 +0300

    Basic func todo list
    Add text
    Delete text

--- Projects/TODO List/assets/js/app.js
+++ Projects/TODO List/assets/js/app.js
@@ -1,20 +1,21 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
-//if li is gray
-//if($(this).css("color") === "rgb(128, 128, 128)") {
-    //turn it black
-    //$(this).css({
-    //    color: "black",
-    //    textDecoration: "none"
-    //});
-//}  
-//else
-//else {
-    //turn it gray
-//    $(this).css({
-//        color: "gray",
-//        textDecoration: "line-through"
-//    });
-//}
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //grabing new todo text from input
+        let todoText = $(this).val();
+        $(this).val("");
+        //create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+});
+
